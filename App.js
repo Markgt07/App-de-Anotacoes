fc5b2826b0dda647011ff7f12061446b81b3bcac
Marcelo <marcelo_louzeiro@hotmail.com>
2021-08-16 15:20:02 -0300

    fase 1 concluida

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View,TouchableOpacity } from 'react-native';
 
 
   
@@ -8,15 +8,77 @@ import { StyleSheet, Text, View } from 'react-native';
   export default function App() {
 
     const  [estado,setEstado] = useState('leitura');
-    const  [anotacao,setAnotacao] = useState('Minha Anotação');
+    const  [anotacao,setAnotacao] = useState('Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição. Lorem Ipsum tem sido o texto fictício padrão da indústria desde 1500, quando um impressor desconhecido pegou um modelo de impressão e embaralhou-o para fazer um livro de amostra de tipos. Ele sobreviveu não apenas cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado. Ele foi popularizado na década de 1960 com o lançamento de folhas de Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker incluindo versões de Lorem Ipsum.');
 
-
+    if(estado == 'leitura'){
     return (
-      <View style={{marginTop:24}}>
-        <Text>{estado}</Text>
+  
+      <View>
+
+        <StatusBar style='light'/>
+        <View style={styles.header}><Text style={{textAlign:'center',color:'white',fontSize:18}}>Aplicativo Anotação</Text></View>
+
+        <View style={{padding:20}}><Text style={styles.anotacao}>{anotacao}</Text></View>
+
+        <TouchableOpacity onPress={()=> setEstado('atualizando')} style={styles.btnAnotacao}><Text style={styles.btnAnotacaoTexto}>+</Text></TouchableOpacity>
         
       </View>
-    );
-  }
 
+    );
+  }else if(estado == 'atualizando'){
+    return(
+    <View>
+
+        <StatusBar style='light'/>
+        <View style={styles.header}><Text style={{textAlign:'center',color:'white',fontSize:18}}>Aplicativo Anotação</Text></View>
+
+        
+
+        <TouchableOpacity onPress={()=> setEstado('leitura')} style={styles.btnAnotacaoSalvar}><Text style={{textAlign:"center",color:'white'}}>salvar</Text></TouchableOpacity>
+        
+      </View>
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+    header:{
+      width:'100%',
+      padding:15,
+      backgroundColor:'#069'
+    },
+    anotacao:{
+      fontSize:13
+    },
+    btnAnotacao:{
+      position:'absolute',
+      right:20,
+      bottom:-450,
+      width: 50,
+      height: 50,
+      backgroundColor:'#069',
+      borderRadius:25
+
+    },
+    btnAnotacaoTexto:{
+      color:'white',
+      position:'relative',
+      textAlign:'center',
+      top:3,
+      fontSize:30
+    },
+    btnAnotacaoSalvar:{
+      position:'absolute',
+      right:20,
+      bottom:-680,
+      width: 50,
+      paddingTop:10,
+      paddingBottom:20,
+      backgroundColor:'#069',
+      borderRadius:25
+
+    },
+    
+
+});
 
